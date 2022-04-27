88d00b31ece06dae5895fe50fe2fd5ea54dabad6
ismaFreemaker <rojasisma10@gmail.com>
2022-04-27 17:00:54 -0300

    changes

---
+++ football-lujan-16-4-2022/football-lujan.js
@@ -0,0 +1,8 @@
+
+let tournamentDate = document.querySelector(".tournament-date");
+tournamentDate.textContent = "16 de Abril 2022";
+
+
+
+let tournamentPlace = document.querySelector(".tournament-place");
+tournamentPlace.textContent = "barrio Rodriguez";
